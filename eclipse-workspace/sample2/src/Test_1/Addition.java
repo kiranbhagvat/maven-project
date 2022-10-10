209e4da5c8618567486000a97207c47f8ec1afac
kiran <kiran@LAPTOP-96G9TDJ3>
2022-10-10 09:27:07 +0530

    fist commite

---
+++ eclipse-workspace/sample2/src/Test_1/Addition.java
@@ -0,0 +1,15 @@
+package Test_1;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		
+		int a=10,b=20;
+		
+		int c;
+		
+		c=a+b;
+		
+		System.out.println("Addition is :"+c);
+	}
+}
