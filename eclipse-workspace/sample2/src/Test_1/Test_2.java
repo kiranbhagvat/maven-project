667521bbd1b975042f9c622b00f481da50ada202
kiran <kiran@LAPTOP-96G9TDJ3>
2022-10-10 09:31:32 +0530

    Second Commite

---
+++ eclipse-workspace/sample2/src/Test_1/Test_2.java
@@ -0,0 +1,15 @@
+package Test_1;
+
+public class Test_2 {
+     
+	public static void main(String[] args) {
+		
+		int a=12, b=2;
+		
+		int c;
+		
+		c=a*b;
+		
+		System.out.println("Multiplication is :"+c);
+	}
+}
